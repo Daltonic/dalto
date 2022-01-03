1ba8d736be2b83d9e34db6079b72d574cb83737f
Daltonic <darlingtongospel@gmail.com>
2022-01-03 11:16:28 +0100

    adds CTA buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ const App = () => {
     <div className="flex flex-col space-y-4 h-screen bg-gray-100">
       <Header />
       <Tabuler />
+      <div className="text-center">
+        <button className="text-white bg-blue-500 py-2 px-5 rounded-xl drop-shadow-xl border border-transparent hover:bg-transparent hover:text-blue-500 hover:border hover:border-blue-500 focus:outline-none focus:ring">
+          Load more
+        </button>
+      </div>
     </div>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,9 +11,14 @@ const Header = () => {
         />
         <span>Dalto</span>
       </div>
-      <nav className="flex flex-row list-none">
+      <nav className="flex flex-row justify-center items-center list-none">
         <li className="cursor-pointer mr-3">Pricing</li>
         <li className="cursor-pointer mr-3">Docs</li>
+        <li className="cursor-pointer mr-3">
+          <button className="text-white bg-blue-500 py-2 px-5 rounded-xl drop-shadow-xl border border-transparent hover:bg-transparent hover:text-blue-500 hover:border hover:border-blue-500 focus:outline-none focus:ring">
+            Send
+          </button>
+        </li>
       </nav>
     </header>
   )
